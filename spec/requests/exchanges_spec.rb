1f2db72c8113af054a074ab94840f26a81e1397c
Tony Matheus <mantony0148@icloud.com>
2018-12-13 08:00:42 -0300

    spec controller

--- spec/requests/exchanges_spec.rb
+++ spec/requests/exchanges_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Exchanges", type: :request do
-  describe "GET /exchanges" do
-    it "works! (now write some real specs)" do
-      get exchanges_index_path
+  describe "GET #index" do
+    it "returns http success" do
+      get '/'
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET #convert' do
+    # before any action in the spec, this code will be ran,
+    # to set @amount some value, that will be used after
+    before do
+      @amount = rand(1...9999)
+    end
+
+    it 'return http success' do
+      get '/convert', params: {
+          source_currency: 'USD',
+          target_currency: 'BRL',
+          amount: @amount
+      }
+
+      expect(response).to have_http_status(200)
+    end
+
+  end
+
 end
