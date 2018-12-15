b0b8500f9dfdf4b8a5261475fdcdff3c432ee60a
Tony Matheus <mantony0148@icloud.com>
2018-12-15 17:36:57 -0300

    medium and hard

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -3,23 +3,51 @@ require 'json'
 include ApplicationHelper
 
 class ExchangeService
-  def initialize(source_currency, target_currency, amount)
-    @source_currency = source_currency
-    @target_currency = target_currency
-    @amount = amount.to_f
+  def initialize
+    @exchange_api_key = credentials(:currency_api_key)
   end
 
-  def perform
+  def perform source_currency, target_currency, amount
     begin
-      exchange_api_url = credentials(:currency_api_url)
-      exchange_api_key = credentials(:currency_api_key)
-      url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
-      res = RestClient.get(url)
-      value = JSON.parse(res.body)['currency'][0]['value'].to_f
+      value = currency_data_request(source_currency, target_currency)
 
-      value * @amount
+      value * amount.to_f
     rescue RestClient::ExceptionWithResponse => e
       e.response
     end
+  end
+
+  def to_bitcoin(target_currency, amount)
+    begin
+      exchange_api_bitcoin = credentials(:currency_api_bitcoin)
+      url = "https://blockchain.info/ticker"
+      p url
+      res = RestClient.get(url)
+      value = JSON.parse(res.body)[target_currency]['last'].to_f
+      value * amount.to_f
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
+  end
+
+  def from_bitcoin(source_currency, amount)
+    begin
+      exchange_api_bitcoin = credentials(:currency_api_bitcoin)
+      url = "https://blockchain.info/ticker"
+      res = RestClient.get(url)
+      value = JSON.parse(res.body)[source_currency]['last'].to_f
+
+      value / amount.to_f
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
+  end
+
+  private
+  def currency_data_request(target_currency, source_currency)
+    exchange_api_url = credentials(:currency_api_url)
+    url = "#{exchange_api_url}?token=#{@exchange_api_key}&currency=#{source_currency}/#{target_currency}"
+    res = RestClient.get(url)
+    JSON.parse(res.body)['currency'][0]['value'].to_f
   end
 end
